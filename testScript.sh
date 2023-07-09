5e218a99d5bbe2ce291a2fd20afcf36c8eb98eaa
Rishu <500061292@upesalumni.upes.ac.in>
2023-07-09 23:17:08 +0530

    This is first commit for shellScript.

---
+++ testScript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello, this is a test for WSL - VS Code for scripting files."
+
+#Get current date and time
+current_date=$(date)
+echo "Date and time of the test is : $current_date"
+
+#This modification is added through VS Code editor
+echo "This line of code is added through VS Code. Test is a success."
+
